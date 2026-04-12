3cac5261d4992fe0e5669e037cfb4201bbcafb66
Vikicha089 <vbanchevf@gmail.com>
2026-04-12 15:44:49 +0300

    Create authMiddleware

---
+++ server/src/middlewares/authMiddleware.ts
@@ -0,0 +1,22 @@
+import { Request, Response, NextFunction } from "express";
+import jwt from 'jsonwebtoken';
+
+export interface AuthRequest extends Request {
+    user?: { id: string };
+}
+
+export const requireAuth = (req: AuthRequest, res: Response, next: NextFunction) => {
+    const token = req.cookies.auth;
+
+    if (!token) {
+        return res.status(401).json({ message: 'Access denied! You are not logged in!' })
+    }
+
+    try {
+        const decoded = jwt.verify(token, 'SECRET_KEY') as { id: string };
+        req.user = decoded;
+        next();
+    } catch (error) {
+        res.status(401).json({message: 'Invalid session. Try again later.'})
+    }
+}
