d43cfee73cf4bdbfa35be7988222f7c32bcf9ca5
Vikicha089 <vbanchevf@gmail.com>
2026-04-12 15:47:19 +0300

    create transaction controller with create and delete

---
+++ server/src/controllers/transactionController.ts
@@ -0,0 +1,42 @@
+import { Request, Router } from "express";
+import { z } from 'zod';
+import { createTransaction, deleteTransaction } from "../services/transactionService";
+import { createTransactionSchema } from "../schemas/transaction.schema";
+import { AuthRequest, requireAuth } from "../middlewares/authMiddleware";
+
+const transactionController = Router();
+
+transactionController.post('/', requireAuth, async (req: AuthRequest, res) => {
+    try {
+        const userId = req.user?.id;
+
+        if (!userId) {
+            return res.status(401).json({ message: 'Unauthorized access' });
+        }
+
+        const validData = createTransactionSchema.parse(req.body);
+        const newTransaction = await createTransaction(validData, userId);
+        res.status(201).json({
+            message: 'Transaction created',
+            transaction: newTransaction
+        })
+    } catch (error: any) {
+        if (error.name === 'ZodError') {
+            res.status(400).json({ errors: error.errors })
+        }
+
+        res.status(500).json({ message: 'Server error' })
+    }
+});
+
+transactionController.delete('/:transactionId', requireAuth, async (req, res) => {
+    try {
+        const transactionId = req.params.transactionId as string;
+        await deleteTransaction(transactionId);
+        res.status(200).json({ message: 'Transaction deleted.' })
+    } catch (error: any) {
+        res.status(500).json({ message: error.message })
+    }
+})
+
+export default transactionController;
