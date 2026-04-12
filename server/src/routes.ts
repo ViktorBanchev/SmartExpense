afbe7d044323179b26c8244c75a41103c431dc32
Vikicha089 <vbanchevf@gmail.com>
2026-04-12 15:47:30 +0300

    add transaction controller route

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import userController from './controllers/userController';
+import transactionController from './controllers/transactionController';
 
 const routes = Router();
 
 routes.use('/users', userController);
+routes.use('/transactions', transactionController);
 
 export default routes;
