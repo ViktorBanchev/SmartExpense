47e8013973b2393ae6b48b44a2bc8e781fd1781a
Vikicha089 <vbanchevf@gmail.com>
2026-04-18 14:38:46 +0300

    Update cookie to use env
    -return logged user information

--- server/src/controllers/userController.ts
+++ server/src/controllers/userController.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 import userService from '../services/userService';
 import { loginSchema, registerSchema } from '../schemas/auth.schema';
+import { config } from '../config';
 
 const userController = Router();
 
@@ -15,9 +16,9 @@ userController.post('/register', async (req, res) => {
 
         res.cookie('auth', token, {
             httpOnly: true,
-            sameSite: 'lax',
-            secure: false,
-            maxAge: 24 * 60 * 60 * 1000
+            sameSite: config.isProduction ? 'none' : 'lax',
+            secure: config.isProduction,
+            maxAge: 10 * 24 * 60 * 60 * 1000
         });
         res.status(201).json({
             message: "User registered"
@@ -35,16 +36,20 @@ userController.post('/register', async (req, res) => {
 userController.post('/login', async (req, res) => {
     try {
         const validData = loginSchema.parse(req.body);
-        const token = await userService.login(validData);
+        const {token, loggedUser} = await userService.login(validData);
 
         res.cookie('auth', token, {
             httpOnly: true,
-            sameSite: 'lax',
-            secure: false,
-            maxAge: 24 * 60 * 60 * 1000
+            sameSite: config.isProduction ? 'none' : 'lax',
+            secure: config.isProduction,
+            maxAge: 10 * 24 * 60 * 60 * 1000
         });
         res.status(200).json({
-            message: "User logged in"
+            message: "User logged in",
+            user: {
+                fullName: loggedUser.fullName,
+                email: loggedUser.email
+            }
         })
 
     } catch (err: any) {
