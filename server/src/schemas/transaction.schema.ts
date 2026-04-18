d3e2f989e57e26b71089be161caad21695085386
Vikicha089 <vbanchevf@gmail.com>
2026-04-18 14:37:25 +0300

    Update transaction.schema.ts

--- server/src/schemas/transaction.schema.ts
+++ server/src/schemas/transaction.schema.ts
@@ -4,8 +4,10 @@ export const createTransactionSchema = z.object({
     // userId: z.string(),
     amount: z.number().positive('Amount should be positive!'),
     type: z.enum(['INCOME', 'EXPENSE'], 'Type can be only income or expense!'),
-    category: z.string(),
+    title: z.string(),
+    // category: z.string(),
     note: z.string().optional(),
+    icon: z.string()
 });
 
 export type CreateTransactionInput = z.infer<typeof createTransactionSchema>;
