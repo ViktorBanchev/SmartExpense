dd846eb8e850a88b0f08e76b9cdd88ce197aab8a
Vikicha089 <vbanchevf@gmail.com>
2026-04-18 18:44:06 +0300

    Add routes to app.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,21 @@
+import { Navigate, Route, Routes } from "react-router"
+import Home from "./pages/Home"
+import Login from "./pages/Login";
+import { useAuthStore } from "./store/authStore";
+import Register from "./pages/Register";
+
 function App() {
-  return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+	const isAuthenticated = useAuthStore((state) => state.isAuthenticated)
+
+	return (
+		<>
+			<Routes>
+				<Route path="/" element={isAuthenticated ? <Home /> : <Navigate to={'/login'} />} />
+				<Route path="/login" element={!isAuthenticated ? <Login /> : <Navigate to={'/'} />} />
+				<Route path="/register" element={!isAuthenticated ? <Register /> : <Navigate to={'/'} />} />
+			</Routes>
+		</>
+	)
 }
 
-export default App
+export default App;
